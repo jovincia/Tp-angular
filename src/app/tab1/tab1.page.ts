a749effa8ce49ddc58d5499b18a5857710382f5f
Jovincia <daniassogba15@gmail.com>
2020-02-03 07:54:51 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,14 +11,17 @@ import { Plugins } from 'protractor/built/plugins';
 })
 export class Tab1Page {
   constructor() {}
+  //Déclaration des variables
   view: string =" ";
   nombre1:number;
   nombre2:number;
-  nombre3:number;
   operateur:string;
   submit(){
+    //declaration du second nombre à utiliser
     this.nombre2=+this.view;
+    //declaration et initialisation de ma variable view 
     this.view=" ";
+    //le switch pour chaque opérateur
     switch(this.operateur){
       case "plus":
         console.log(this.nombre1+"+"+this.nombre2);
@@ -56,23 +59,25 @@ export class Tab1Page {
 
     }
   }
+  //la fonction clear"AC"
 
   clear(){
     this.view=" ";
     log(this.view);
   }
-  
-
+  //la fonction click pour afficher un nombre à l'écran
   click(nbr:number){
     this.view=this.view + nbr;
     log(this.view);
   }
+  //la fonctin plus
   plus(){
     this.nombre1 =+ this.view;
     this.operateur = "plus";
     this.view="";
     console.log(this.view);
   }
+  //la fonction moins
   moins(){
     this.nombre1 =+ this.view;
     this.operateur = "moins";
@@ -80,24 +85,28 @@ export class Tab1Page {
     console.log(this.view);
 
   }
+  //la fonction de la multiplication
   multi(){
   this.nombre1 =+ this.view;
   this.operateur = "multi";
   this.view="";
   console.log(this.view);
   }
+  //la fonction de la division
   division(){
     this.nombre1 =+ this.view;
     this.operateur = "division";
     this.view="";
     console.log(this.view);
     }
+    //la fonction modulo
     modulo(){
         this.nombre1 =+ this.view;
         this.operateur = "modulo";
         this.view="";
         console.log(this.view);
         }
+        //la fonction plusoumoins
         plus_moins(pm:number){
           pm=+this.view;
           if(pm >= 0){
@@ -114,6 +123,7 @@ export class Tab1Page {
           }
           
     }
+    //la fonction qui gère la virgule 
     point(nbr1:number){
      
       nbr1=+this.view;
@@ -122,10 +132,5 @@ export class Tab1Page {
       console.log(this.view);
 
     }
-    
-  
-  
-
-
 }
 
